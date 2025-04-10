c763623a765d47f000431be14416fe9a2e82b5bd
Firdaus <firdaus1337@gmail.com>
2025-04-11 02:18:42 +0800

    feat: header, footer, and page structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage(){
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4">About Me</h1>
+            <p>About Me.Bruh later I will add</p>
+        </div>
+    )
+}
